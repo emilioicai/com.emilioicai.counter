bcdaacaec4f488f4bbd8d33ab6fe3d0bc960706e
emilioicai <emiliorodriguez@gmail.com>
2015-05-27 16:52:55 +0200

    new styling

--- controllers/widget.js
+++ controllers/widget.js
@@ -1,7 +1,8 @@
 var initialValue = value = 2;
-var numberHeight = 50;
+var numberHeight = 80;
 var cb;
 var scrollUnit;
+var centerFactor = 1.95;
 
 _.each($.count.children, function(child){
 	child.height = numberHeight;
@@ -9,22 +10,22 @@ _.each($.count.children, function(child){
 
 $.count.addEventListener('postlayout', function(){
 	scrollUnit = (OS_IOS) ? numberHeight : numberHeight * (Ti.Platform.displayCaps.dpi / 160);
-	$.count.scrollTo(0, scrollUnit/2 + scrollUnit);
+	$.count.scrollTo(0, scrollUnit * centerFactor);
 });
 
-$.shader.image = WPATH('/shader.png');
+// $.shader.image = WPATH('/shader.png');
 
 function increaseCount(){
 	if(value === $.count.children.length - 2) return;
 	++value;
-	$.count.scrollTo(0, scrollUnit/2 + scrollUnit * (value - 2) + scrollUnit);
+	$.count.scrollTo(0, scrollUnit * (value - 2) + scrollUnit * centerFactor);
 	if(typeof cb === "function") cb(value);
 }
 
 function decreaseCount(){
 	if(value === 1) return;
 	--value;
-	$.count.scrollTo(0, scrollUnit/2 + scrollUnit * (value - 2) + scrollUnit );
+	$.count.scrollTo(0, scrollUnit + scrollUnit * (value - 2) + scrollUnit * centerFactor / 2 );
 	if(typeof cb === "function") cb(value);
 }
 
